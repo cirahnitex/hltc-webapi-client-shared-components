14f1e46dd7123a83c4a239eab83c8245585255d0
YAN Yuchen <cirahnitex@gmail.com>
2019-02-26 21:06:56 +0800

    withDaemon now can accept async function as daemon task

--- src/components/withDaemon.tsx
+++ src/components/withDaemon.tsx
@@ -2,16 +2,29 @@ import * as React from 'react';
 
 type ComponentConstructor<TProps> = React.ComponentClass<TProps> | React.StatelessComponent<TProps>;
 
-export function withDaemon<Props>(Comp:ComponentConstructor<Props>, daemonTask:()=>void, interval:number) {
+const delay = (ms:number)=>new Promise((resolve)=>setTimeout(resolve, ms));
+
+/**
+ * wrap a component which while being attached to DOM, keep executing a daemon task with an interval
+ * @param Comp the component to wrap
+ * @param daemonTask the task to repeatedly be executed
+ * @param interval the time interval between execution (ms)
+ */
+export function withDaemon<Props>(Comp:ComponentConstructor<Props>, daemonTask:()=>Promise<void>|void, interval:number) {
     return class extends React.PureComponent<Props,{}> {
-        refreshDaemon = 0;
+        shouldStop = false;
         componentWillMount() {
-            daemonTask();
-            if(!this.refreshDaemon) this.refreshDaemon = window.setInterval(daemonTask, interval);
+            (async()=>{
+                while(true) {
+                    await daemonTask();
+                    if(this.shouldStop) break;
+                    await delay(interval);
+                    if(this.shouldStop) break;
+                }
+            })();
         }
         componentWillUnmount() {
-            clearInterval(this.refreshDaemon);
-            this.refreshDaemon = 0;
+            this.shouldStop = true;
         }
         render() {
             return <Comp {...this.props}/>
